7a7a5abfa32174ae3ea49a608649e061de451e8a
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-23 13:52:36 +0200

    10-203 -> Add Logo component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
+  
   render() {
+   
     return (
-      <div className='app'>
-        logo
+    
+    <div className='app'>
+        <Logo />
       </div>
-    );
+    
+  );
   }
 }
