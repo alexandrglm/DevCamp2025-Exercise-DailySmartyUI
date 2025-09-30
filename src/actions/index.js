16bace33b57e91ff22cbd2d95ad332d72b305ec2
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-30 02:53:08 +0200

    Fix 'El Problema de la linea desaparecida' in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from './types.js'
 
-const API_URL = 'https://projectfive-devcamp-api-275615f66dc4.herokuapp.com/'
+
 
 export function fetchRecentPosts()  {
 
@@ -26,6 +26,8 @@ export function fetchRecentPosts()  {
 }
 
 export function fetchPostsWithQuery(query, callback){
+
+    const API_URL = process.env.REACT_APP_API_URL
 
     return function(dispatch){
 
